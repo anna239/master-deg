a5bcf55af303bee88a22c7c4eff4b9c6674ee04a
anna.zhdan <anna.zhdan@jetbrains.com>
2013-04-29 17:11:52 +0400

    added project with annotations

---
+++ immutability/annotations/src/org/jetbrains/immutability/annotations/Const.java
@@ -0,0 +1,8 @@
+package org.jetbrains.immutability.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD})
+public @interface Const {
+}
---
+++ immutability/annotations/src/org/jetbrains/immutability/annotations/Immutable.java
@@ -0,0 +1,8 @@
+package org.jetbrains.immutability.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE, ElementType.FIELD, ElementType.METHOD, ElementType.LOCAL_VARIABLE, ElementType.PARAMETER})
+public @interface Immutable {
+}
